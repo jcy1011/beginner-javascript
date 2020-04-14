74955fd0a27aede012aa6d2adb37ac0b53dda186
Jonathan Yale <jonathanyale@gmail.com>
2020-04-14 16:59:43 -0400

    added solutions to function questions

--- exercises/20 - The DOM/DOM-Cardio.js
+++ exercises/20 - The DOM/DOM-Cardio.js
@@ -79,12 +79,38 @@ div.firstElementChild.remove();
 //   <p>They are HEIGHT and AGE years old. In Dog years this person would be AGEINDOGYEARS. That would be a tall dog!</p>
 // </div>
 
+function generatePlayerCard(name, age, height) {
+  AGEINDOGYEARS = Math.floor(age / 7);
+  return `
+    <div class="playerCard">
+      <h2>${name} — ${age}</h2>
+      <p>They are ${height} and ${age} years old. In Dog years this person would be ${AGEINDOGYEARS}. That would be a tall dog!</p>
+    </div>
+  `;
+}
+
 // make a new div with a class of cards
+const playerDiv = document.createElement('div');
+playerDiv.classList = 'cards';
 
 // Have that function make 4 cards
+const joe = generatePlayerCard('Joe', 33, `6'1"`);
+const jim = generatePlayerCard('Jim', 45, `6'7"`);
+const sally = generatePlayerCard('Sally', 23, `5'1"`);
+const sam = generatePlayerCard('Sam', 76, `5'8"`);
+
+const joeFrag = document.createRange().createContextualFragment(joe);
+const jimFrag = document.createRange().createContextualFragment(jim);
+const sallyFrag = document.createRange().createContextualFragment(sally);
+const samFrag = document.createRange().createContextualFragment(sam);
 
 // append those cards to the div
+playerDiv.appendChild(joeFrag);
+playerDiv.appendChild(jimFrag);
+playerDiv.appendChild(sallyFrag);
+playerDiv.appendChild(samFrag);
 // put the div into the DOM just before the wrapper element
+myDiv.insertAdjacentElement('beforebegin', playerDiv);
 // Bonus, put a delete Button on each card so when you click it, the whole card is removed
 
 // select all the buttons!
