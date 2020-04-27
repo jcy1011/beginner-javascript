31c0ebf5e85eb9c2a788f201d752bb92f1c1085b
Jonathan Yale <jonathanyale@gmail.com>
2020-04-27 13:59:09 -0400

    added notes and working functionality to scroll to accept

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,1 +1,28 @@
-console.log('IT WORKS!');
+console.log('IT WORKS... BRO!');
+
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing button so not running in background. Free up CPUs
+    ob.unobserve(terms.lastElementChild);
+  }
+  //   this along with removing unobserve above would make button appear
+  //   and reappear every time you scroll to and from it
+  //   could be useful for images but not here
+  //   else {button.disabled = true;}
+}
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1,
+});
+
+ob.observe(terms.lastElementChild);
+
+// using scroll events is the old way of doing it. Not going into detail
+// terms.addEventListener('scroll', function(e) {
+//   console.log(e.currentTarget);
+// });
