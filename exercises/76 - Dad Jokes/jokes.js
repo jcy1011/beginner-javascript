139431ea59f8c07449f69b0ea6e00c48ec76ca0d
Jonathan Yale <jonathanyale@gmail.com>
2020-06-17 14:31:22 -0400

    add notes and complete code

--- exercises/76 - Dad Jokes/jokes.js
+++ exercises/76 - Dad Jokes/jokes.js
@@ -1,5 +1,7 @@
 const jokeButton = document.querySelector('.getJoke');
+const jokeButtonSpan = jokeButton.querySelector('.jokeText');
 const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
 
 const buttonText = [
   'Ugh.',
@@ -13,6 +15,8 @@ const buttonText = [
 ];
 
 async function fetchJoke() {
+  // turn loader on
+  loader.classList.remove('hidden');
   const response = await fetch('https://icanhazdadjoke.com/', {
     headers: {
       Accept: 'application/json',
@@ -20,13 +24,32 @@ async function fetchJoke() {
   });
   const data = await response.json();
   // console.log(data);
+  // turn loader off
+  loader.classList.add('hidden');
   return data;
+}
+
+// NOTE: we keep this a general utility function for potential use with many
+// things by passing in arr and not hardcoding buttonText
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  // prevent same thing from being returned consecutively
+  if (item === not) {
+    console.log('Look again');
+    return randomItemFromArray(arr, not);
+  }
+  return item;
 }
 
 async function handleClick() {
   // destructure data we want into joke variable. We keep other data in case we need
   const { joke } = await fetchJoke();
-  console.log(joke);
+  // console.log(joke);
+  jokeHolder.textContent = joke;
+  jokeButtonSpan.textContent = randomItemFromArray(
+    buttonText,
+    jokeButtonSpan.textContent
+  );
 }
 
 jokeButton.addEventListener('click', handleClick);
